0fa83be9c644b1707e14e021cb3f1c3ab89b6cfc
Dasha Burgos <dasha.lewis@gmail.com>
2023-03-09 21:40:14 -0500

    completed all arrow functions in step 10

--- app.js
+++ app.js
@@ -158,8 +158,9 @@ console.log(joe);
 
 
 
-Student.prototype.greeting = () => `Hi, my name is ${this.name}`;
-;
+Student.prototype.greeting = function() {
+  return `Hi, my name is ${this.name}`;
+};
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
@@ -169,12 +170,11 @@ console.log(joe.greeting());
 
 
 
-Student.courseName = function() {
-  return 'This student is enrolled in Code 301.';
-};
+Student.courseName =() => 'This student is enrolled in Code 301.';
+
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
+console.log(Student.courseName());
 
 
 
